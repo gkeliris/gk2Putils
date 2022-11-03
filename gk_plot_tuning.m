aca9f7372ab5c08dc74b440d508f049b47ff5935
Georgios A. Keliris <keliris@gmail.com>
2022-11-03 15:07:09 -0400

    update 3Nov22 b

--- gk_plot_tuning.m
+++ gk_plot_tuning.m
@@ -17,11 +17,11 @@ if nargin == 3
 end
 
 Ntrials = size(sig.trials_ONresp,2); 
-sigMeanON = squeeze(nanmean(sig.trials_ONresp(roiNum,:,:),2));
-sigSemON = squeeze(nanstd(sig.trials_ONresp(roiNum,:,:),0,2)./sqrt(Ntrials));
+sigMeanON = squeeze(mean(sig.trials_ONresp(roiNum,:,:),2));
+sigSemON = squeeze(std(sig.trials_ONresp(roiNum,:,:),0,2)./sqrt(Ntrials));
 
-sigMeanOFF= squeeze(nanmean(sig.trials_OFFresp(roiNum,:,:),2));
-sigSemOFF = squeeze(nanstd(sig.trials_OFFresp(roiNum,:,:),0,2)./sqrt(Ntrials));
+sigMeanOFF= squeeze(mean(sig.trials_OFFresp(roiNum,:,:),2));
+sigSemOFF = squeeze(std(sig.trials_OFFresp(roiNum,:,:),0,2)./sqrt(Ntrials));
 
 errorbar(stimValues,sigMeanON,sigSemON,'ko-','MarkerFaceColor',[0 0 0]);
 yline([0],'--')
