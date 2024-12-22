aacf5b80fa4e74898338e401119afdac407d30fd
Georgios Keliris <keliris@gmail.com>
2024-12-22 16:29:46 -0500

    added loadStat

--- utils/loadStat.m
+++ utils/loadStat.m
@@ -1,14 +1,13 @@
-function stat = loadSig(ds, sigName, plane)
-% USAGE: sig = loadSig(ds, sigName, [plane])
+function stat = loadStat(ds, plane)
+% USAGE: sig = loadStat(ds, [plane])
 %
 % INPUT:
 %   ds          - table returned by gk_datasetQuery
-%   sigName     - 'F','Fneu', 'spks', 'iscell',...
 %   plane       - default: 'combined', 'plane0', 'plane1',..., or  0, 1
 %
 % Author: Georgios A. Keliris
 
-if nargin<3
+if nargin<2
     plane='combined';
 end
 
@@ -17,10 +16,10 @@ if isnumeric(plane)
 end
 
 sesPath = setSesPath(ds);
-if ~isfile(fullfile(sesPath,'suite2p_orig',plane,[sigName,'.npy']))
+if ~isfile(fullfile(sesPath,'suite2p_orig',plane,'stat.npy'))
     error('.npy FILE NOT FOUND! Make sure dataset was preprocessed and try again.\n')
     return
 end
-sig = readNPY(fullfile(sesPath,'suite2p_orig',plane,[sigName,'.npy']));
+stat = readNPY(fullfile(sesPath,'suite2p_orig',plane,'stat.npy'));
 
 
