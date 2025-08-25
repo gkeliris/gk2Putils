a616a970e6ab09a49e74195c465f5c0c79675d18
Georgios Keliris <keliris@gmail.com>
2025-08-25 16:31:13 -0400

    updated loadMultiTiff.m to skip typecast to int16

--- utils/loadMultiTiff.m
+++ utils/loadMultiTiff.m
@@ -11,11 +11,15 @@ for i=1:numel(d)
     fprintf('Loading file: %s\n',d(i).name)
     try
         data=ct(3,data,parallelReadTiff(fullfile(d(i).folder,d(i).name)));
+        tc=false;
     catch
+        tc=true;
         data=cat(3,data,read_file(fullfile(d(i).folder,d(i).name)));
     end
     toc
 end
 %fprintf('Converting to int16\n')
-data = reshape(typecast(data(:),'int16'),[size(data)]);
+if tc
+    data = reshape(typecast(data(:),'int16'),[size(data)]);
+end
 toc
